eca83e5c887450d048444ab6270d97ed6e59ffd7
KTDev <katrynsteenkamp0@gmail.com>
2025-10-22 22:54:59 +0200

    student dashboard backend connect

---
+++ frontend/src/chatbot/types.js
@@ -0,0 +1,12 @@
+// src/types.js
+
+/**
+ * @typedef {Object} Message
+ * @property {string} text - The message text.
+ * @property {'user' | 'bot'} sender - The sender of the message.
+ * @property {string[]} [suggestedReplies] - Optional suggested replies for the user.
+ * @property {{ name: string, type: string }} [file] - Optional file attached to the message.
+ */
+
+
+export {}; 
